e7d6f565d5b10fb740f56469f6f5266f607d7f5d
I Flagg <isaiahnflagg@gmail.com>
2020-11-19 23:20:55 -0500

    added javascript variables

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+
+const numberButtons = document.querySelector("[data-number]")
+const operationButtons = document.querySelector("[data-operation]")
+const equalsButton = document.querySelector("[data-equals]")
+const deleteButton = document.querySelector("[data-delete]")
+const allClearButton = document.querySelector("[data-all-clear]")
+const previousOperandTextElement = document.querySelector("[data-previous-operand]")
+const currentOperandTextElement = document.querySelector("[data-current-operand")
