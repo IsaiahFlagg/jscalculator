365b6676e36335938abb0d0f44726f0ab52d8125
I Flagg <isaiahnflagg@gmail.com>
2020-11-20 01:46:22 -0500

    JavaScript file is done

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ class Calculator {
     clear() {
         this.currentOperand = ""
         this.previousOperand = ""
-        this.Operand = undefined
+        this.operation = undefined
+
     }
 
     delete() {
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     appendNumber(number) {
@@ -51,15 +52,40 @@ class Calculator {
             default:
                 return
         }
-        
+
         this.currentOperand = computation
         this.operation = undefined
         this.previousOperand = ""
     }
 
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split(".")[0])
+        const decimalDigits = stringNumber.split(".")[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ""
+        } else {
+            integerDisplay = integerDigits.toLocaleString("en", {
+                maximumFractionDigits: 0
+            })
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
+
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.currentOperand
-        this.previousOperandTextElement.innerText = this.previousOperand
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText =
+                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+
+        } else {
+            this.previousOperandTextElement.innerText = ""
+        }
     }
 }
 
@@ -97,5 +123,15 @@ operationButtons.forEach(button => {
 
 $(equalsButton).on("click", function () {
     calculator.compute()
+    calculator.updateDisplay()
+})
+
+$(allClearButton).on("click", function () {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+$(deleteButton).on("click", function () {
+    calculator.delete()
     calculator.updateDisplay()
 })
