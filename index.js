a0457dfc5802e1f4fcf9c60890dfa2dc56c8e152
I Flagg <isaiahnflagg@gmail.com>
2020-11-19 23:53:43 -0500

    calculator functionality added

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+    
+    clear() {
+        this.currentOperand = ""
+        this.previousOperand = ""
+        this.Operand = undefined
+    }
 
+    delete() {
+
+    }
+
+    appendNumber(number) {
+        this.currentOperand = number
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand
+    }
+}
 
 
 const numberButtons = document.querySelector("[data-number]")
@@ -8,3 +40,21 @@ const deleteButton = document.querySelector("[data-delete]")
 const allClearButton = document.querySelector("[data-all-clear]")
 const previousOperandTextElement = document.querySelector("[data-previous-operand]")
 const currentOperandTextElement = document.querySelector("[data-current-operand")
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+// numberButtons.forEach(element => console.log(element));
+numberButtons.forEach(button => {
+    // $(button).on("click", function() {
+    //     calculator.appendNumber(button.innerText)
+    //     calculator.updateDisplay()
+    // });
+    button.addEventListener("click", () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+
+
+})
+
+console.log(number);
